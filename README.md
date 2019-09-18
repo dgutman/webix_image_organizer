b4871cb0b0325b368ed035123eb588277ce296f4
JC <jvizcar@emory.edu>
2019-09-18 15:31:41 -0400

    transplant host added, simple favicon to avoid error message

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Image Organizer app that uses the Webix Toolkit to interact with a Girder server
+
+Steps:
+1. npm install
+2. npm run start
+
+Adding new hosts:
+1. Open webpack.config.js for editing (found in root of repo)
+2. add a new item in SERVER_LIST (bottom of page)
+3. example: {id: "#", value: "A name", hostAPI: "http://servername:8080/api/v1"}
--- webpack.config.js
+++ webpack.config.js
@@ -91,8 +91,9 @@ module.exports = function (env) {
 			new webpack.EnvironmentPlugin({
 				SERVER_LIST: [
 					{id: "1", value: "Girder", hostAPI: "http://dermannotator.org:8080/api/v1"},
-					{id: "2", value: "Cancer digital slide archive", hostAPI: "http://candygram.neurology.emory.edu:8080/api/v1"}
-					// {id: "3", value: "ISIC Archive", hostAPI: "https://sandbox.isic-archive.com/api/v1"}
+					{id: "2", value: "Cancer digital slide archive", hostAPI: "http://candygram.neurology.emory.edu:8080/api/v1"},
+					{id: "3", value: "Transplant", hostAPI: "http://transplant.digitalslidearchive.emory.edu:8080/api/v1"},
+					//{id: "3", value: "ISIC Archive", hostAPI: "https://sandbox.isic-archive.com/api/v1"}
 				]
 			})
 		]
