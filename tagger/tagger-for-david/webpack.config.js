7bf2f9a1d03246d988d6b50598e70d813f9974e0
David Gutman <dagutman@gmail.com>
2020-01-24 20:41:54 +0000

    changing default port to 8081 to not conflict with girder

--- tagger/tagger-for-david/webpack.config.js
+++ tagger/tagger-for-david/webpack.config.js
@@ -75,7 +75,7 @@ module.exports = function (env) {
 		},
 		devServer: {
 			host: process.env.DEV_HOST || "localhost",
-			port: process.env.DEV_PORT || 8080,
+			port: process.env.DEV_PORT || 8081,
 			// Path to all other files (e.g. index.html and webix):
 			contentBase: ["./codebase", "./node_modules"],
 			inline: true
