a39a905d85b2c5cae3237d33e3b4af9001f1c114
baturian <anch.batura@gmail.com>
2021-05-15 22:05:58 +0300

    refactor: removed unused modules

--- faceted_search/server/api/extensions/filters_images.js
+++ faceted_search/server/api/extensions/filters_images.js
@@ -1,14 +1,9 @@
-const is = require('is_js');
 const facetFilters = require('../models/facet_filters');
 const facetImages = require('../models/facet_images');
 
-// const facetFilters = new FacetFilters();
-// const facetImages = new FacetImages();
-
-
 const getFiltersWithImages = (req, res) => {
-    let map = {};
-    let result = {
+    const map = {};
+    const result = {
         "filters": {},
         "facets": {
 
@@ -16,18 +11,18 @@ const getFiltersWithImages = (req, res) => {
     };
     facetFilters.getAll()
         .then((data) => {
-            for(let i of data) {
+            for(const i of data) {
                 result.filters[i.id] = {
                     "name": i.name,
                     "values": i.options,
                     "type": i.type
-                }
+                };
             }
             return facetImages.getAll();
         })
         .then((images) => {
-            for(let i of images) {
-                for(let j of i.facets) {
+            for(const i of images) {
+                for(const j of i.facets) {
                     if(!result.filters.hasOwnProperty(j.id)) {
                         if(!result.facets.hasOwnProperty(j.id)) {
                             result.facets[j.id] = {
@@ -45,7 +40,7 @@ const getFiltersWithImages = (req, res) => {
             }
         })
         .then(()=> res.status(200).send(result))
-        .catch((err) => res.status(500).send(err))
+        .catch((err) => res.status(500).send(err));
 };
 
 module.exports = getFiltersWithImages;
