3152417d89b4b0c90c98fedffec932c147812e5f
David Gutman <dagutman@gmail.com>
2022-07-15 16:55:20 -0400

    moving more functions to helper lib

--- microarray/tmaHelpers.py
+++ microarray/tmaHelpers.py
@@ -1,5 +1,92 @@
 import matplotlib.pyplot as plt
 import random
+import cv2
+import numpy as np
+from matplotlib.patches import Rectangle
+
+def plotGrid( w, h, x1, y1, theta, baseImage,colLabels, rowLabels ):
+    ### Draws a grid of width w and height h starting at x1,y1 with a rotation of theta degrees
+    cmap = get_cmap( len(colLabels) * len(rowLabels))
+
+
+    plt.rcParams["figure.figsize"] = (15,15)
+    plt.imshow(baseImage)
+    for i,c in enumerate(colLabels):
+    #print("Row: %s" % r)
+        for j,r in enumerate(rowLabels):## j is the index, c is the rowLabel
+        #print("\t",r,c)
+
+            x = w*i+x1
+            y = h*j+y1
+            
+            (xc,yc) = rotate((x,y),(x1,y1),theta)
+            ### Compute the corrected coordinated after rotation
+            
+            plt.gca().add_patch(Rectangle((xc,yc),w,h,
+                        angle=theta,
+#                         edgecolor=cmap( random.randint(0,len(colLabels) * len(rowLabels))),
+                        edgecolor=cmap( i*len(colLabels)+j),
+                        facecolor='none',
+                        lw=2))
+            
+    #plot fiducial for upper left 
+    plt.plot(x1,y1, marker='v', color="red")
+
+    ## For debugging below, also extract a sample TMA Core
+    
+    # tmaCore = region_im[y1:(y1+h),x1:(x1+w),:]    
+
+
+def calculateImgCentroid( img, thr, useOTSU=True,useBlur=True):
+    """Expects an image as a numpy array and a threshold T and computes the centroid of the image
+    This also uses OTSU thresholding and a gaussian blur prior to computing the centroid by default
+    
+    """
+    ## To do is first check if image is gray scale or single channel or not..
+       
+    
+    if len(img.shape)<3:
+        gray = img
+    else:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+
+    if useOTSU and useBlur:
+
+        ret,threshImg  = cv2.threshold(gray,127,255,0)
+    else:
+        ret,threshImg  = cv2.threshold(gray,127,255,0)
+    
+
+
+    M = cv2.moments(threshImg)
+    try:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+    except:
+        print("Centroid calc failed")
+        cX=0
+        cY=0
+
+    return cX, cY
+
+
+
+def rotate(point, origin, degrees):
+    radians = np.deg2rad(degrees)
+    x,y = point
+    offset_x, offset_y = origin
+    adjusted_x = (x - offset_x)
+    adjusted_y = (y - offset_y)
+    cos_rad = np.cos(radians)
+    sin_rad = np.sin(radians)
+    qx = offset_x + cos_rad * adjusted_x + sin_rad * adjusted_y
+    qy = offset_y + -sin_rad * adjusted_x + cos_rad * adjusted_y
+    return qx, qy
+
+
 
 def generate_random_color():
     ### Will return a string like rgba with 3 random ints between 0 and 255..
