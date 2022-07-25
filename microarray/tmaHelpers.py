783960647a771a96977381f40f7fd38da4bc532d
David Gutman <dagutman@gmail.com>
2022-07-25 16:38:29 -0400

    working on region export code

--- microarray/tmaHelpers.py
+++ microarray/tmaHelpers.py
@@ -3,64 +3,71 @@ import random
 import cv2
 import numpy as np
 from matplotlib.patches import Rectangle
-from ipywidgets import interact,FloatSlider,IntSlider,interactive,HBox,VBox, fixed
-
-def plotGrid( w, h, x1, y1, theta, baseImage,colLabels, rowLabels ):
-    ### Draws a grid of width w and height h starting at x1,y1 with a rotation of theta degrees
-    cmap = get_cmap( len(colLabels) * len(rowLabels))
+from ipywidgets import interact, FloatSlider, IntSlider, interactive, HBox, VBox, fixed
 
 
-    plt.rcParams["figure.figsize"] = (15,15)
+def plotGrid(w, h, x1, y1, theta, baseImage, colLabels, rowLabels,annotationElements):
+    # Draws a grid of width w and height h starting at x1,y1 with a rotation of theta degrees
+    cmap = get_cmap(len(colLabels) * len(rowLabels))
+    annotationElements.clear()#  = [] ### Reset the array
+    
+    sf = 32  # Scale Factor
+    # print()
+    plt.rcParams["figure.figsize"] = (15, 15)
     plt.imshow(baseImage)
-    for i,c in enumerate(colLabels):
-    #print("Row: %s" % r)
-        for j,r in enumerate(rowLabels):## j is the index, c is the rowLabel
-        #print("\t",r,c)
-
+    for i, c in enumerate(colLabels):
+        #print("Row: %s" % r)
+        for j, r in enumerate(rowLabels):  # j is the index, c is the rowLabel
+            # print("\t",r,c)
             x = w*i+x1
             y = h*j+y1
+            (xc, yc) = rotate((x, y), (x1, y1), theta)
+            # Compute the corrected coordinated after rotation
             
-            (xc,yc) = rotate((x,y),(x1,y1),theta)
-            ### Compute the corrected coordinated after rotation
+            aec = cmap(i*len(colLabels)+j) ## Annotation Element Color
             
-            plt.gca().add_patch(Rectangle((xc,yc),w,h,
-                        angle=theta,
-#                         edgecolor=cmap( random.randint(0,len(colLabels) * len(rowLabels))),
-                        edgecolor=cmap( i*len(colLabels)+j),
-                        facecolor='none',
-                        lw=2))
-            
-    #plot fiducial for upper left 
-    plt.plot(x1,y1, marker='v', color="red")
-
-    ## For debugging below, also extract a sample TMA Core
-    
-    # tmaCore = region_im[y1:(y1+h),x1:(x1+w),:]    
+            annotationElements.append({'fillColor': 'rgba(0, 0, 0, 0)',
+                                       'label': {"value": "%s%s" % (r, c)},
+                                       'lineColor': 'rgba(%d,%d,%d,%d)' % (aec[0]*255,aec[1]*255,aec[2]*255,aec[3]),
 
 
-def calculateImgCentroid( img, thr, useOTSU=True,useBlur=True):
+                                       'lineWidth': 1,
+                                       'type': 'rectangle',
+                                       'center': [(xc+w/2)*sf, (yc+h/2)*sf, 0],
+                                       'width': w*sf,
+                                       'height': h*sf,
+                                       })
+
+            plt.gca().add_patch(Rectangle((xc, yc), w, h,
+                                          angle=theta,
+                                          #                         edgecolor=cmap( random.randint(0,len(colLabels) * len(rowLabels))),
+                                          edgecolor=cmap(i*len(colLabels)+j),
+                                          facecolor='none',
+                                          lw=2))
+
+    print(len(annotationElements))
+
+    # plot fiducial for upper left
+    plt.plot(x1, y1, marker='v', color="red")
+    # For debugging below, also extract a sample TMA Core
+    # tmaCore = region_im[y1:(y1+h),x1:(x1+w),:]
+
+def calculateImgCentroid(img, thr, useOTSU=True, useBlur=True):
     """Expects an image as a numpy array and a threshold T and computes the centroid of the image
     This also uses OTSU thresholding and a gaussian blur prior to computing the centroid by default
-    
     """
-    ## To do is first check if image is gray scale or single channel or not..
-       
-    
-    if len(img.shape)<3:
+    # To do is first check if image is gray scale or single channel or not..
+    if len(img.shape) < 3:
         gray = img
     else:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
     blurred = cv2.GaussianBlur(gray, (7, 7), 0)
 
     if useOTSU and useBlur:
-
-        ret,threshImg  = cv2.threshold(gray,127,255,0)
+        ret, threshImg = cv2.threshold(gray, 127, 255, 0)
     else:
-        ret,threshImg  = cv2.threshold(gray,127,255,0)
-    
-
+        ret, threshImg = cv2.threshold(gray, 127, 255, 0)
 
     M = cv2.moments(threshImg)
     try:
@@ -68,16 +75,13 @@ def calculateImgCentroid( img, thr, useOTSU=True,useBlur=True):
         cY = int(M["m01"] / M["m00"])
     except:
         print("Centroid calc failed")
-        cX=0
-        cY=0
-
+        cX = 0
+        cY = 0
     return cX, cY
-
-
 
 def rotate(point, origin, degrees):
     radians = np.deg2rad(degrees)
-    x,y = point
+    x, y = point
     offset_x, offset_y = origin
     adjusted_x = (x - offset_x)
     adjusted_y = (y - offset_y)
@@ -87,25 +91,22 @@ def rotate(point, origin, degrees):
     qy = offset_y + -sin_rad * adjusted_x + cos_rad * adjusted_y
     return qx, qy
 
-
-
 def generate_random_color():
-    ### Will return a string like rgba with 3 random ints between 0 and 255..
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    return 'rgb(%d,%d,%d)' % (r,g,b)   
+    # Will return a string like rgba with 3 random ints between 0 and 255..
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return 'rgb(%d,%d,%d)' % (r, g, b)
 
 def get_cmap(n, name='hsv'):
     '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
 
-
-def get_annotation_region(gc, item_id, annotationElement,mag=None):
-    #left=None, top=None, height=None, width=None, right=None, bottom=None, mag=None, rgb=True, pad=None):
+def get_annotation_region(gc, item_id, annotationElement, mag=None):
+    # left=None, top=None, height=None, width=None, right=None, bottom=None, mag=None, rgb=True, pad=None):
     """Get a region of a WSI image from an annotation element that is stored in the DSA...
-    
+
     Parameters
     ----------
     gc : girder_client.GirderClient
@@ -136,42 +137,35 @@ def get_annotation_region(gc, item_id, annotationElement,mag=None):
 
     aeType = annotationElement['type']
 
-    if ( aeType == 'rectangle'):
-
+    if (aeType == 'rectangle'):
         print(annotationElement)
-        ### For a rectangle, we have to compute the left as we are just given the center coord
-        left = annotationElement['center'][0] - annotationElement['width'] /2
-        top = annotationElement['center'][1] - annotationElement['height'] /2
+        # For a rectangle, we have to compute the left as we are just given the center coord
+        left = annotationElement['center'][0] - annotationElement['width'] / 2
+        top = annotationElement['center'][1] - annotationElement['height'] / 2
 
-        right = annotationElement['center'][0] + annotationElement['width'] /2
-        bottom = annotationElement['center'][1] + annotationElement['height'] /2
-
-
-
+        right = annotationElement['center'][0] + annotationElement['width'] / 2
+        bottom = annotationElement['center'][1] + \
+            annotationElement['height'] / 2
     else:
         print("Annotation element type %s is not supported currently for export" % aeType)
 
     encoding = 'TILED'
-
     if mag is None:
         get_url = f'{item_id}/tiles/region?left={left}&top={top}&right={right}&bottom={bottom}&units=base_pixels' \
-                f'&exact=false&encoding={encoding}&jpegQuality=100&jpegSubsampling=0'
+            f'&exact=false&encoding={encoding}&jpegQuality=100&jpegSubsampling=0'
     else:
         get_url = f'{item_id}/tiles/region?left={left}&top={top}&right={right}&bottom={bottom}&units=base_pixels' \
-                f'&magnification={mag}&exact=false&encoding={encoding}&jpegQuality=100&jpegSubsampling=0'
+            f'&magnification={mag}&exact=false&encoding={encoding}&jpegQuality=100&jpegSubsampling=0'
     # else:
     #     raise Exception('You must pass width / height or right / bottom parameters to get a region')
     print(get_url)
-    
     resp_content = gc.get('item/' + get_url, jsonResp=False).content
     #region_im = np.array(Image.open(BytesIO(resp_content)))
-
     return resp_content
-
 
     # def plotGrid( w, h, x1, y1, theta, baseImage ):
 #     ### Draws a grid of width w and height h starting at x1,y1 with a rotation of theta degrees
-    
+
 #     plt.rcParams["figure.figsize"] = (15,15)
 #     plt.imshow(baseImage)
 #     for i,c in enumerate(colLabels):
@@ -181,35 +175,63 @@ def get_annotation_region(gc, item_id, annotationElement,mag=None):
 
 #             x = w*i+x1
 #             y = h*j+y1
-            
+
 #             (xc,yc) = tma.rotate((x,y),(x1,y1),theta)
 #             ### Compute the corrected coordinated after rotation
-            
+
 #             plt.gca().add_patch(Rectangle((xc,yc),w,h,
 #                         angle=theta,
 # #                         edgecolor=cmap( random.randint(0,len(colLabels) * len(rowLabels))),
 #                         edgecolor=cmap( i*len(colLabels)+j),
 #                         facecolor='none',
 #                         lw=2))
-            
-#     #plot fiducial for upper left 
+
+#     #plot fiducial for upper left
 #     plt.plot(x1,y1, marker='v', color="red")
 
 #     ## For debugging below, also extract a sample TMA Core
-    
-#     tmaCore = region_im[y1:(y1+h),x1:(x1+w),:]    
+
+#     tmaCore = region_im[y1:(y1+h),x1:(x1+w),:]
+# x=1
+# y=1
+# x1 = int(tmaWidth*x+topLeft[0])
+# y1 = int(tmaHeight*y+topLeft[1])
+# w =  int(tmaWidth)
+# h = int(tmaHeight)
+# print(x1,y1,w,h)
+# plt.imshow(region_im[y1:(y1+h),x1:(x1+w),:])
+
+# tmaCore = region_im[y1:(y1+h),x1:(x1+w),:]
 
 
-def generateTMAcontrols( rowLabels, colLabels, baseImage):
-    ### Generates several ipywidgets to control the width, height, theta, x1/y1
-    ### which are used to create an interactive ipywidget
+def updateAutoTMA(imgId, annotationElements, gc, replaceExisting=False):
+    """This will update the autoTMA I am generating in jupyter notebooks
+    and optionally delete the older version"""
+    tmaAe = {
+    "name": "autoTMA",              # Non-empty string.  Optional
+    "description": "This is a description",  # String.  Optional
+    "elements": annotationElements                           # A list.  Optional.
+                                            # See below for valid elements.
+    }
+    gc.post('annotation?itemId=%s' % imgId,json=tmaAe)
 
-    maxTmaWidth = baseImage.shape[1]/ len(colLabels)
-    maxTmaHeight = baseImage.shape[0]/ len(rowLabels)
-    tmaWidth_s = IntSlider(min=10,max=maxTmaWidth, step=1, value=100,continuous_update=False)
-    tmaHeight_s = IntSlider(min=50,max=maxTmaHeight, step=1, value=100,continuous_update=False)
-    theta_s = FloatSlider(min=-10,max=10,step=0.2, value=0,continuous_update=False)
-    leftX_s = IntSlider(min=10,max=maxTmaWidth*2, step=1, value=10,continuous_update=False)
-    leftY_s = IntSlider(min=10,max=maxTmaHeight*2, step=1, value=10,continuous_update=False)
+def generateTMAcontrols(rowLabels, colLabels, baseImage):
+    # Generates several ipywidgets to control the width, height, theta, x1/y1
+    # which are used to create an interactive ipywidget
 
-    return (tmaWidth_s,tmaHeight_s, theta_s,leftX_s,leftY_s)
+    maxTmaWidth = baseImage.shape[1] / len(colLabels)
+    maxTmaHeight = baseImage.shape[0] / len(rowLabels)
+    tmaWidth_s = IntSlider(min=10, max=maxTmaWidth, step=1,
+                           value=100, continuous_update=False)
+    tmaHeight_s = IntSlider(min=50, max=maxTmaHeight,
+                            step=1, value=100, continuous_update=False)
+    theta_s = FloatSlider(min=-10, max=10, step=0.2,
+                          value=0, continuous_update=False)
+    leftX_s = IntSlider(min=10, max=maxTmaWidth*2, step=1,
+                        value=10, continuous_update=False)
+    leftY_s = IntSlider(min=10, max=maxTmaHeight*2, step=1,
+                        value=10, continuous_update=False)
+
+    return (tmaWidth_s, tmaHeight_s, theta_s, leftX_s, leftY_s)
+
+
