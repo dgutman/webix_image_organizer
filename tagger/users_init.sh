f4d002cbd83f86a126366b3da56cc9fab8daa93f
volishevko <volishevko@xbsoftware.by>
2022-08-17 16:28:56 +0300

    fix: update script to create database

--- tagger/users_init.sh
+++ tagger/users_init.sh
@@ -6,26 +6,26 @@ set -e
 # admin user is expected to be already created when this script executes. We use it here to authenticate as admin to create
 # dbUser and databases.
 
-echo ">>>>>>> trying to create database and users"
-if [ -n "${MONGO_INITDB_ROOT_USERNAME:-}" ] && [ -n "${MONGO_INITDB_ROOT_PASSWORD:-}" ] && [ -n "${MONGODB_USER:-}" ] && [ -n "${MONGODB_PASSWORD:-}" ]; then
+echo `>>>>>>> trying to create database and users`
+if [ -n `${MONGO_INITDB_ROOT_USERNAME:-}` ] && [ -n `${MONGO_INITDB_ROOT_PASSWORD:-}` ] && [ -n `${MONGODB_USER:-}` ] && [ -n `${MONGODB_PASSWORD:-}` ]; then
 mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD<<EOF
 
 
 
-use '$MONGODB_DATABASE';
+use $MONGODB_DATABASE
 db['Test'].insertOne({'HELLO':'WORLD'})
 
 db.createUser({
-  user:  '$MONGODB_USER',
-  pwd: '$MONGODB_PASSWORD',
-  roles: [{
-    role: 'readWrite',
-    db: '$MONGODB_DATABASE'
+  'user':  '$MONGODB_USER',
+  'pwd': '$MONGODB_PASSWORD',
+  'roles': [{
+    'role': 'readWrite',
+    'db': '$MONGODB_DATABASE'
   }]
-});
+})
 
 
-#use admin;
+#use admin
 #db.createUser({
 #  user:  '$MONGODB_USER',
 #  pwd: '$MONGODB_PASSWORD',
@@ -33,10 +33,11 @@ db.createUser({
 #    role: 'admin',
 #    db: 'admin'
 #  }]
-#});
+#})
 
 EOF
 else
-    echo "MONGODB_INITDB_ROOT_USERNAME,MONGODB_INITDB_ROOT_PASSWORD,MONGODB_USER and MONGODB_PASSWORD must be provided. Some of these are missing, hence exiting database and user creatioin"
+    echo `MONGODB_INITDB_ROOT_USERNAME,MONGODB_INITDB_ROOT_PASSWORD,MONGODB_USER and MONGODB_PASSWORD must be provided. Some of these are missing, hence exiting database and user creatioin`
     exit 403
 fi
+echo `>>>>>>>>>>>>>>> finish create database and users`
