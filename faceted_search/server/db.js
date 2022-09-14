c135a6f02d9102d58a677b3957fa8a1f5c843a56
Slava Olishevko <volishevko@xbsoftware.by>
2022-09-14 11:39:36 +0300

    fix: fix docker config

--- faceted_search/server/db.js
+++ faceted_search/server/db.js
@@ -4,10 +4,19 @@ const mongo = require('../config').mongo;
 
 module.exports = function(callback) {
 	const uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.name}`;
+	const mongoOptions = {
+		useUnifiedTopology: true,
+		useNewUrlParser: true
+	};
 
 	// Use bluebird
 	mongoose.Promise = Promise;
-	mongoose.connect(uri);
+	mongoose.connect(uri, mongoOptions);
+	const db = mongoose.connection;
+	db.on("error", console.error.bind(console, "Connection error:"));
+	db.once("open", () => {
+		console.log("Mongoose connection is open");
+	});
 
 	callback();
 };
